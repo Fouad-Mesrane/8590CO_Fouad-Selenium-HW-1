6aaaff0a750ab854a4a44e347718b918a8a9e0f2
Fouad-Mesrane <fouadmesrane@gmail.com>
2022-10-28 21:54:46 -0600

    Added TestAuthentication

--- src/test/java/test_authentication/TestAuthentication.java
+++ src/test/java/test_authentication/TestAuthentication.java
@@ -1,2 +1,15 @@
-package test_authentication;public class TestAuthentication {
+package test_authentication;
+
+import app_page_library.HomePage;
+import base.BasePage;
+import config.Config;
+import org.testng.annotations.Test;
+
+public class TestAuthentication extends BasePage {
+
+    @Test
+    public void authentication(){
+        HomePage homePage = new HomePage();
+        homePage.login(Config.properties.getProperty("app_user"),Config.properties.getProperty("app_password") );
+    }
 }
